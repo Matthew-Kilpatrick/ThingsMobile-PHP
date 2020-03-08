1d60c70054b1da4e058c489613caf6a51ab3accb
Matthew Kilpatrick <16194494+Matthew-Kilpatrick@users.noreply.github.com>
2020-03-08 22:16:44 +0000

    Added working status and list endpoints

--- src/ThingsMobilePHP/Remote/Endpoint/Sim.php
+++ src/ThingsMobilePHP/Remote/Endpoint/Sim.php
@@ -60,12 +60,36 @@ class Sim extends Endpoint
 
   public function status(\ThingsMobilePHP\Models\Sim $sim) : \ThingsMobilePHP\Models\Sim
   {
-
+    $client = $this->getHttpClient();
+    $body = $this->getSimAuthArray($sim);
+    $req = $client->request('POST', $this->client->getApiBaseUrl() . 'simStatus', [
+      'form_params' => $body
+    ]);
+    if ($this->parseNoneDataXml($req->getBody()))
+    {
+      return $this->simModelFromXmlResponse($req->getBody())[0];
+    }
   }
 
   public function list($search=[]) : array // search for name or tag
   {
-
+    $client = $this->getHttpClient();
+    $body = $this->getAuthArray();
+    if (isset($search['name']))
+    {
+      $body['name'] = $search['name'];
+    }
+    if (isset($serach['tag']))
+    {
+      $body['tag'] = $search['tag'];
+    }
+    $req = $client->request('POST', $this->client->getApiBaseUrl() . 'simList', [
+      'form_params' => $body
+    ]);
+    if ($this->parseNoneDataXml($req->getBody()))
+    {
+      return $this->simModelFromXmlResponse($req->getBody());
+    }
   }
 
   public function update(\ThingsMobilePHP\Models\Sim $sim) : bool
@@ -73,4 +97,35 @@ class Sim extends Endpoint
     // will combine 3.8 [name update], 3.9 [tag update], 3.10 [expiration date], 3.11 [traffic threshold], 3.15 [associate plan] API endpoints if the associated parameter is modified
   }
 
+  private function simModelFromXmlResponse($response) : array
+  {
+    $sims = [];
+    $xml = simplexml_load_string($response);
+    foreach ($xml->sims as $simData) {
+      $simData = $simData->sim;
+      $sim = new \ThingsMobilePHP\Models\Sim();
+      $sim->setActivationDate(new \DateTime((string)$simData->activationDate)) // TODO: if not activated, this is null
+      ->setBalance((int)$simData->balance)
+        ->setBlockSimAfterExpirationDate((bool)$simData->blockSimAfterExpirationDate)
+        ->setBlockSimDaily((bool)$simData->blockSimDaily)
+        ->setBlockSimMonthly((bool)$simData->blockSimMonthly)
+        ->setBlockSimTotal((bool)$simData->blockSimTotal)
+        ->setDailyTraffic((int)$simData->dailyTraffic)
+        ->setDailyTrafficThreshold((int)$simData->dailyTrafficThreshold)
+        ->setExpirationDate(new \DateTime((string)$simData->expirationDate)) // TODO: if never expires, this is null
+        ->setIccid((string)$simData->iccid)
+        ->setLastConnectionDate(new \DateTime((string)$simData->lastConnectionDate)) // TODO: if never connected, this is null
+        ->setMonthlyTraffic((int)$simData->monthlyTraffic)
+        ->setMsisdn((string)$simData->msisdn)
+        ->setName((string)$simData->name)
+        ->setplan((string)$simData->plan)
+        ->setStatus((string)$simData->status == 'active')
+        ->setType((string)$simData->type)
+        ->setTag((string)$simData->tag)
+        ->setTotalTraffic((int)$simData->totalTraffic)
+        ->setTotalTrafficThreshold((int)$simData->totalTrafficThreshold);
+      $sims[] = $sim;
+    }
+    return $sims;
+  }
 }
